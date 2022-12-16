12f5810bd973e58daaee442bb9f8bd1669b5666e
Divine6747 <Divine.Okhaus@students.ittralee.ie>
2022-12-16 16:12:36 +0000

    Signed-off-by: Divine6747 <Divine.Okhaus@students.ittralee.ie>

--- Level Game project £D/Assets/PlayerMovement.cs
+++ Level Game project £D/Assets/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody playerRB;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,30 +17,43 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown("space"))
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float VerticalMovement = Input.GetAxis("Vertical");
+
+        playerRB.velocity = new Vector3(horizontalMovement * movementSpeed, playerRB.velocity.y, VerticalMovement * movementSpeed);
+     
+       if (Input.GetButton("Jump"))
+       {
+            playerRB.velocity = new Vector3(playerRB.velocity.x, jumpForce, playerRB.velocity.z);
+       } 
+
+       /*
+       if (Input.GetKeyDown("space"))   
         {
-            playerRB.velocity = new Vector3(0, 5, 0);
+            playerRB.velocity = new Vector3(0, 3f, 0);
 
         }
         if (Input.GetKey("up"))
-            playerRB.velocity = new Vector3(0, 0, 5);
+            playerRB.velocity = new Vector3(0, 0, 5f);
         {
 
         }
         if (Input.GetKey("right"))
-            playerRB.velocity = new Vector3(5, 0, 0);
+            playerRB.velocity = new Vector3(5f, 0, 0);
         {
 
         }
         if (Input.GetKey("down"))
-            playerRB.velocity = new Vector3(0, 0, -5);
+            playerRB.velocity = new Vector3(0, 0, -5f);
         {
 
         }
         if (Input.GetKey("left"))
-            playerRB.velocity = new Vector3(-5, 0, 0);
+            playerRB.velocity = new Vector3(-5f, 0, 0);
         {
 
         }
+            */
+        
     }
 }
